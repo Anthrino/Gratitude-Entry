f36db0455606fdedf5a60a3e02b3847bc5b34096
Jerin John <jerin.j@somaiya.edu>
2017-12-07 12:18:22 +0530

    First Draft.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {MatButtonModule, MatInputModule, MatFormFieldModule, MatCardModule} from '@angular/material';
+
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
